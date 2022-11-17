ea7c13a21683c595f7b9db221e4815912a6fa8e4
NishantSambyal <nishantsambyal123@gmail.com>
2022-11-17 23:57:33 +0530

    Login Screen Design Completed

--- src/screens/LoginScreen/Login.js
+++ src/screens/LoginScreen/Login.js
@@ -1,4 +1,12 @@
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React, {Component} from 'react';
 import {logo} from '../../assets';
 
@@ -6,12 +14,38 @@ import {logo} from '../../assets';
 export class Login extends Component {
   render() {
     return (
-      <View style={styles.mainComponent}>
-        <View style={styles.logoWrapper}>
-          <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        <View style={styles.mainComponent}>
+          <View style={styles.logoWrapper}>
+            <Image style={styles.logo} source={logo} />
+          </View>
+          <Text style={styles.title}>Log-in</Text>
+
+          <View style={{marginTop: 30}}>
+            <Text style={styles.label}>Email</Text>
+
+            <TextInput placeholder="Your email ID" style={styles.input} />
+
+            <TextInput />
+          </View>
+
+          <View style={{marginTop: 26}}>
+            <Text style={styles.label}>Password</Text>
+
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              style={styles.input}
+            />
+
+            <TextInput />
+          </View>
+
+          <TouchableOpacity style={styles.loginWrapper}>
+            <Text style={styles.loginButton}>Login</Text>
+          </TouchableOpacity>
         </View>
-        <Text style={styles.title}>Log-in</Text>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -21,8 +55,8 @@ export class Login extends Component {
 const styles = StyleSheet.create({
   mainComponent: {
     flex: 1, // to capture the share in the given window
-    // justifyContent: 'center', // vertical
-    // alignItems: 'center', // horizontal
+    // // justifyContent: 'center', // vertical
+    // // alignItems: 'center', // horizontal
     margin: 16,
   },
   logoWrapper: {
@@ -36,6 +70,29 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     fontWeight: '700',
+    marginBottom: 40,
+  },
+  label: {
+    fontSize: 16,
+  },
+  input: {
+    margin: 8,
+    borderBottomWidth: 1,
+    padding: 10,
+    borderBottomColor: 'grey',
+  },
+
+  loginWrapper: {
+    marginTop: 60,
+    borderRadius: 16,
+    paddingVertical: 15,
+    paddingHorizontal: 80,
+    backgroundColor: 'rgb(0, 60, 120)',
+    alignSelf: 'center',
+  },
+  loginButton: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 export default Login;
